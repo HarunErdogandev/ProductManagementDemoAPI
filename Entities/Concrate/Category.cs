e73b36c96378df25fbb43e7782463b89c7cb7abd
Coder <Coder@DESKTOP-9PMBFOV>
2023-06-09 00:40:24 +0300

    Add project files.

---
+++ Entities/Concrate/Category.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrate
+{
+    public  class Category:IEntity
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+}
